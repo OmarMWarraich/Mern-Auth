71e7f05ac8a922f11843fd3cb9e6b12dd7534d80
OmarMWarraich <earthling986@gmail.com>
2023-01-29 17:56:49 +0500

    feat: Pull Routes into Server.js and Test Using RapidApi

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
 
 const app = express();
 
 // BodyParser middleware
-
 app.use(
     bodyParser.urlencoded({
         extended: false
@@ -27,6 +29,15 @@ mongoose
     )
     .then(() => console.log("MongoDB connected successfully"))
     .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+app.get('/', (req, res)=> res.send('Hello World!'));
+// Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000; //process.env.port is the deployed port
 
