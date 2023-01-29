710affb5cad89425474de68ffde965c82447e589
OmarMWarraich <earthling986@gmail.com>
2023-01-29 21:42:31 +0500

    feat: Add Register and Login Components into App

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 
 const App = () => {
   return (
     <div className='App'>
       <Router>
-        <Routes>
-            <Route path="/" element={<Navbar />} />
-            <Route path="/landing" element={<Landing />} />
-        </Routes>
+          <div className='App'>
+            <Navbar />
+            <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
       </Router>
     </div>
   );
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,123 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+    
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [password2, setPassword2] = useState('');
+
+
+    const HandleChange = (e) => {
+        const {name, value} = e.target;
+        switch(name){
+            case 'name':
+                setName(value);
+                break;
+            case 'email':
+                setEmail(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            case 'password2':
+                setPassword2(value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    const HandleSubmit = (e) => {
+        e.preventDefault();
+        const newUser = {
+            name,
+            email,
+            password,
+            password2
+        };
+        console.log(newUser);
+    }
+
+    return (
+        <>  
+            <div className="container">
+        <div className="row">
+          <div className="col s8 offset-s2">
+            <Link to="/" className="btn-flat waves-effect">
+              <i className="material-icons left">keyboard_backspace</i> Back to
+              home
+            </Link>
+            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+              <h4>
+                <b>Register</b> below
+              </h4>
+              <p className="grey-text text-darken-1">
+                Already have an account? <Link to="/login">Log in</Link>
+              </p>
+            </div>
+            <form noValidate onSubmit={HandleSubmit}>
+              <div className="input-field col s12">
+                <input
+                  onChange={HandleChange}
+                  name="name"
+                  value={name}
+                  id="name"
+                  type="text"
+                />
+                <label htmlFor="name">Name</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={HandleChange}
+                  name="email"
+                  value={email}
+                  id="email"
+                  type="email"
+                />
+                <label htmlFor="email">Email</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={HandleChange}
+                  name="password"
+                  value={password}
+                  id="password"
+                  type="password"
+                />
+                <label htmlFor="password">Password</label>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={HandleChange}
+                  name="password2"
+                  value={password2}
+                  id="password2"
+                  type="password"
+                />
+                <label htmlFor="password2">Confirm Password</label>
+              </div>
+              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem"
+                  }}
+                  type="submit"
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                >
+                  Sign up
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+        </>
+    )       
+}
+
+export default Register;
