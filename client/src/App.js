8bb9fe2b69e7586fe3d0c9108a5cdd3abc270739
OmarMWarraich <earthling986@gmail.com>
2023-01-29 19:14:29 +0500

    feat: Add and Import Landing and Navbar components

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 
 const App = () => {
   return (
-    <div className='App'><h1>Hello</h1></div>
-  )
+    <div className='App'>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Navbar />} />
+            <Route path="/landing" element={<Landing />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App
